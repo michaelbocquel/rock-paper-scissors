e2db993077c5de0a57c6010fbb30af970081177a
nosca <michael.bocquel@gmail.com>
2024-02-03 11:54:49 +0100

    added all html + all js working as expected, still design to do + optimize js

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ let playerSelection = ""
 let computerScore = 0
 let playerScore = 0
 
-function getRandomComputerChoice() {
-	let randomNumber = Math.floor(Math.random() * 3)
-	computerSelection = choices[randomNumber]
-}
-
 const rockButton = document.querySelector('.rock-button')
 const paperButton = document.querySelector('.paper-button')
 const scissorsButton = document.querySelector('.scissors-button')
@@ -17,9 +12,20 @@ const randomButton = document.querySelector('.random-button')
 const roundSummaryContainer = document.querySelector('.round-summary-container')
 const scoreContainer = document.querySelector('.score-container')
 
+const winnerContainer = document.querySelector('.winner-container')
+const playAgainButton = document.querySelector('.play-again-button')
+
+winnerContainer.classList.add('is-hidden')
+playAgainButton.classList.add('is-hidden')
+
 rockButton.addEventListener("click", () => playOneRound("Rock"))
 paperButton.addEventListener("click", () => playOneRound("Paper"))
 scissorsButton.addEventListener("click", () => playOneRound("Scissors"))
+
+function getRandomComputerChoice() {
+	let randomNumber = Math.floor(Math.random() * 3)
+	computerSelection = choices[randomNumber]
+}
 
 function playOneRound(playerSelection) {
 	getRandomComputerChoice()
@@ -40,4 +46,29 @@ function playOneRound(playerSelection) {
 		playerScore++
 	}
 	scoreContainer.textContent = `Player ${playerScore} - ${computerScore} Computer`
-}
+	if (playerScore === 5 || computerScore === 5) {
+		winnerContainer.classList.remove('is-hidden')
+		playAgainButton.classList.remove('is-hidden')
+		rockButton.disabled = true
+		paperButton.disabled = true
+		scissorsButton.disabled = true
+	}
+	if (playerScore === 5) {
+		winnerContainer.textContent = 'Player won'
+	} else if (computerScore === 5) {
+		winnerContainer.textContent = 'Computer won'
+	}
+	playAgainButton.onclick = () => {
+		computerSelection = ""
+		playerSelection = ""
+		computerScore = 0
+		playerScore = 0
+		rockButton.disabled = false
+		paperButton.disabled = false
+		scissorsButton.disabled = false
+		winnerContainer.classList.add('is-hidden')
+		playAgainButton.classList.add('is-hidden')
+		roundSummaryContainer.textContent = ''
+		scoreContainer.textContent = ''
+	}
+} 
